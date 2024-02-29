bbf7306dabeebc0792be8cf916b9560dbce984c6
y2456y16 <y2456y16@gmail.com>
2024-02-29 16:46:37 +0900

    [modify] prefab 수정

--- Assets/Externals/KCW/scripts/ProfileControl.cs
+++ Assets/Externals/KCW/scripts/ProfileControl.cs
@@ -5,6 +5,14 @@ using UnityEngine.UI;
 
 public class ProfileControl : MonoBehaviour
 {
+    private Text playerhp;
+    private Text playerStamina;
     private Text attackStatTxt;
+    private Text fireRate;
 
+    private void Start()
+    {
+        attackStatTxt.text = Managers.PlayerStats.W_Atk.ToString();
+        attackStatTxt.text = Managers.PlayerStats.W_FireRate.ToString();
+    }
 }
