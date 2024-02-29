42d35953f37978b88151850a881d5cc70aa287a0
pure-night <58593218+pure-night@users.noreply.github.com>
2024-02-29 20:27:33 +0900

    [Feat] Player LowPoly

--- Assets/Scripts/Manager/GameSceneManager.cs
+++ Assets/Scripts/Manager/GameSceneManager.cs
@@ -9,7 +9,7 @@ public class GameSceneManager : MonoBehaviour
     //현재 임시 코드
     public void InitializeGameScene()
     {
-        _playerPrefab = Resources.Load<GameObject>("Prefabs/Player");
+        _playerPrefab = Resources.Load<GameObject>("Prefabs/PlayerLowPoly");
         Player = Instantiate(_playerPrefab, new Vector3(0, 1, 0), Quaternion.Euler(Vector3.zero));
     }
 }
