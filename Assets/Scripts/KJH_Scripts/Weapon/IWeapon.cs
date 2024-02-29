0a3065a300ae0a93113427dc184b41fc07b36f8e
문정현 <58593218+pure-night@users.noreply.github.com>
2024-02-29 16:06:43 +0900

    Revert "Feature/Player Additional feat"

--- Assets/Scripts/KJH_Scripts/Weapon/IWeapon.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IWeapon 
-{
-    void Attack(Vector3 origin, Quaternion rotation);
-    // void SpawnCase(Transform caseSpawnPoint);
-}
--- Assets/Scripts/Manager/Managers.cs
+++ Assets/Scripts/Manager/Managers.cs
@@ -4,69 +4,15 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
-    private static Managers _instance;
-    public static Managers Instance { get { Initialize(); return _instance; } }
-    
-    private PoolManager _poolManager;
-    private ResourceManager _resourceManager;
-    private PlayerStatsManager _playerStatsManager;
-    private AttackManager _attackManager;
-    
-    public static PoolManager Pool => Instance._poolManager;
-    public static ResourceManager RM => Instance._resourceManager;
-    public static AttackManager Attack => Instance._attackManager;
-    public static PlayerStatsManager PlayerStats => Instance._playerStatsManager;
-    
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    public static void Execute()
+    // Start is called before the first frame update
+    void Start()
     {
-        Initialize();
-    }
-    
-    private static void Initialize()
-    {
-        if (_instance == null)
-        {
-            var go = GameObject.Find("@Managers");
-
-            if (go == null)
-            {
-                go = new GameObject("@Managers");
-                go.AddComponent<Managers>();
-            }
-            
-            DontDestroyOnLoad(go);
-            _instance = go.GetComponent<Managers>();
-
-            if (!go.TryGetComponent(out _instance._poolManager))
-            {
-                _instance._poolManager = go.AddComponent<PoolManager>();
-                Pool.Init();
-            }
-
-            if (!go.TryGetComponent(out _instance._resourceManager))
-            {
-                _instance._resourceManager = go.AddComponent<ResourceManager>();
-            }
-
-            if (!go.TryGetComponent(out _instance._attackManager))
-            {
-                _instance._attackManager = go.AddComponent<AttackManager>();
-                Attack.Init();
-            }
-
-            if (!go.TryGetComponent(out _instance._playerStatsManager))
-            {
-                _instance._playerStatsManager = go.AddComponent<PlayerStatsManager>();
-                PlayerStats.Init();
-                PlayerStats.WeaponSetup();
-            }
-        }
+        
     }
 
-    public static void Clear()
+    // Update is called once per frame
+    void Update()
     {
-        PlayerStats.Clear();
-        Pool.Clear();
+        
     }
 }
