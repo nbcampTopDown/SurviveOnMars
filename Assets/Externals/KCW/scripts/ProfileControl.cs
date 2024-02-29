1ad35cecc688cdc558b17f8f267bff6b80058413
y2456y16 <y2456y16@gmail.com>
2024-02-29 17:43:08 +0900

    [modify] profile control create

--- Assets/Externals/KCW/scripts/ProfileControl.cs
+++ Assets/Externals/KCW/scripts/ProfileControl.cs
@@ -5,14 +5,15 @@ using UnityEngine.UI;
 
 public class ProfileControl : MonoBehaviour
 {
-    private Text playerhp;
-    private Text playerStamina;
-    private Text attackStatTxt;
-    private Text fireRate;
+
+    [SerializeField] private Text playerhp;
+    [SerializeField] private Text playerStamina;
+    [SerializeField] private Text attackStatTxt;
+    [SerializeField] private Text fireRate;
 
     private void Start()
     {
         attackStatTxt.text = Managers.PlayerStats.W_Atk.ToString();
-        attackStatTxt.text = Managers.PlayerStats.W_FireRate.ToString();
+        fireRate.text = Managers.PlayerStats.W_FireRate.ToString();
     }
 }
