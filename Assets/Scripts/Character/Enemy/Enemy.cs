b503a1f8e353e19079254d7f2b9ab140c0aae9d7
pure-night <58593218+pure-night@users.noreply.github.com>
2024-03-04 04:59:21 +0900

    [Feat] Melee Enemy

--- Assets/Scripts/Character/Enemy/Enemy.cs
+++ Assets/Scripts/Character/Enemy/Enemy.cs
@@ -1,25 +1,153 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour, IDamageable
 {
     [field: SerializeField] public CharacterHealth CharacterHealth { get; private set; }
+    [field: SerializeField] public float IdleMoveSpeedMultiplier { get; private set; }
+    [field: SerializeField] public float IdleLocationRadius { get; private set; }
+    [field: SerializeField] public Animator Animator { get; private set; }
+    public Transform Player { get; set; }
+    public float updateRate = 0.1f;
+    private Collider _collider;
+    private NavMeshAgent _agent;
+    private Coroutine _followCoroutine;
+
+    public EnemyState defaultState;
+    private EnemyState _state;
+    private readonly int _speedHash = Animator.StringToHash("Speed");
+    private readonly int _dieHash = Animator.StringToHash("Die");
+
+    public EnemyState State
+    {
+        get => _state;
+        set
+        {
+            OnStateChangeEvent?.Invoke(_state, value);
+            _state = value;
+        }
+    }
+    public event Action<EnemyState, EnemyState> OnStateChangeEvent;
 
     private void Awake()
     {
+        _collider = GetComponent<Collider>();
+        _agent = GetComponent<NavMeshAgent>();
+        Player = Managers.GameSceneManager.Player.transform;
+        
+        OnStateChangeEvent += HandleStateChange;
+        
+        State = defaultState;
+    }
+
+    private void Update()
+    {
+        Animator.SetFloat(_speedHash, _agent.velocity.magnitude / 3.5f, 0.5f, Time.deltaTime);
+    }
+
+    private void OnEnable()
+    {
+        CharacterHealth.Health = 100;
         CharacterHealth.OnDie += OnDie;
+        _collider.enabled = true;
+        Spawn();
+    }
+
+    private void OnDisable()
+    {
+        State = EnemyState.Spawn;
+        _state = defaultState;
+    }
+
+    public void Spawn()
+    {
+        OnStateChangeEvent?.Invoke(EnemyState.Spawn, defaultState);
+    }
+
+    private void HandleStateChange(EnemyState oldState, EnemyState newState)
+    {
+        if(oldState == newState)
+            return;
+
+        if (_followCoroutine != null)
+        {
+            StopCoroutine(_followCoroutine);
+        }
+
+        if (oldState == EnemyState.Idle)
+        {
+            _agent.speed /= IdleMoveSpeedMultiplier;
+        }
+
+        switch (newState)
+        {
+            case EnemyState.Idle:
+                _followCoroutine = StartCoroutine(DoIdleMotion());
+                break;
+            case EnemyState.Chase:
+                _followCoroutine = StartCoroutine(FollowTarget());
+                break;
+        }
+    }
+
+    private IEnumerator DoIdleMotion()
+    {
+        var wait = new WaitForSeconds(updateRate);
+        var stopping = new WaitForSeconds(2f);
+
+        _agent.speed *= IdleMoveSpeedMultiplier;
+
+        while (true)
+        {
+            if (!_agent.enabled || !_agent.isOnNavMesh)
+            {
+                yield return wait;
+            }
+            else if(_agent.remainingDistance <= _agent.stoppingDistance)
+            {
+                yield return stopping;
+                var point = Random.insideUnitCircle * IdleLocationRadius;
+                NavMeshHit hit;
+
+                if (NavMesh.SamplePosition(_agent.transform.position + new Vector3(point.x, 0, point.y), out hit, 2f,
+                        _agent.areaMask))
+                {
+                    _agent.SetDestination(hit.position);
+                }
+            }
+            yield return wait;
+        }
+    }
+
+    private IEnumerator FollowTarget()
+    {
+        var wait = new WaitForSeconds(updateRate);
+
+        while (gameObject.activeSelf)
+        {
+            if (_agent.enabled)
+            {
+                _agent.SetDestination(Player.position);
+            }
+
+            yield return wait;
+        }
     }
 
     public void TakeDamage(float damage)
     {
-        Debug.Log(damage);
         CharacterHealth.TakeDamage(damage);
     }
 
     private void OnDie()
     {
-        Debug.Log("die");
+        CharacterHealth.OnDie -= OnDie;
+        StopCoroutine(_followCoroutine);
+        _agent.SetDestination(this.transform.position);
+        _collider.enabled = false;
+        Animator.SetTrigger(_dieHash);
     }
 }
--- Assets/Scripts/Manager/GameSceneManager.cs
+++ Assets/Scripts/Manager/GameSceneManager.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class GameSceneManager : MonoBehaviour
 {
-    private GameObject _playerPrefab;
     public GameObject Player { get; private set; }
 
     //TODO GameScene로딩할 때, 필요한 오브젝트 로딩
     //현재 임시 코드
     public void InitializeGameScene()
     {
-        _playerPrefab = Resources.Load<GameObject>("Prefabs/PlayerLowPoly");
-        Player = Instantiate(_playerPrefab, new Vector3(0, 1, 0), Quaternion.Euler(Vector3.zero));
-        Managers.UI_Manager.ShowUI<UI_HUD>();
+        // 맵 생성
+        var mapPrefab = Resources.Load<GameObject>("Prefabs/TerrainMap");
+        var mapInfo = Instantiate(mapPrefab).GetComponent<MapInfo>();
+        
+        var playerPrefab = Resources.Load<GameObject>("Prefabs/PlayerLowPoly");
+        Player = Instantiate(playerPrefab, mapInfo.PlayerSpawnPoint.position, Quaternion.Euler(Vector3.zero));
+        UI_Manager.instance.ShowUI<UI_HUD>();
+
+        var enemyPrefab = Resources.Load<GameObject>("Prefabs/Enemy");
+        Instantiate(enemyPrefab);
+        Instantiate(enemyPrefab);
+        Instantiate(enemyPrefab);
     }
 }
