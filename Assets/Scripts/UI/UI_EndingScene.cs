73153a498cd060f620bc9ea67cc1948741613dce
sangmin-1007 <dosin0822@gmail.com>
2024-03-04 19:03:57 +0900

    modify : Ending UI에서 연출 추가
    -END- 글씨 Fadein 연출 추가

--- Assets/Scripts/UI/UI_EndingScene.cs
+++ Assets/Scripts/UI/UI_EndingScene.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using TMPro;
+using UnityEngine.UIElements;
 
 public class UI_EndingScene : UI_Base<UI_EndingScene>
 {
     [SerializeField] private RawImage cinematicMovie;
     [SerializeField] private RectTransform backGround;
+
+    [SerializeField] private TextMeshProUGUI endText;
 
     public override void OnEnable()
     {
@@ -23,11 +27,18 @@ public class UI_EndingScene : UI_Base<UI_EndingScene>
     {
         cinematicMovie.DOFade(1f, 5f);
         yield return new WaitForSeconds(27f);
+
         cinematicMovie.DOFade(0f, 8f);
-        yield return new WaitForSeconds(9f);
+        yield return new WaitForSeconds(8f);
 
         Managers.SoundManager.ChangeBackGroundMusic(Managers.RM.Load<AudioClip>("Sounds/BGM/EndingSceneBGM"));
         backGround.DOAnchorPosY(2860f,40f);
+        yield return new WaitForSeconds(40f);
+
+        endText.DOFade(1f, 8f);
+        yield return new WaitForSeconds(18f);
+
+        UnityEditor.EditorApplication.isPlaying = false;
         yield break;
 
     }
