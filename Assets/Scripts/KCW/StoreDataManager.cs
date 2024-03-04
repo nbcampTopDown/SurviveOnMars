70528fa50e7753ee5c1953dc5db6d451df4db46f
y2456y16 <y2456y16@gmail.com>
2024-03-04 12:01:36 +0900

    [modify] item data modifier

--- Assets/Scripts/KCW/StoreDataManager.cs
+++ Assets/Scripts/KCW/StoreDataManager.cs
@@ -3,29 +3,107 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum IsCharacterStat
+{
+    hp,
+    stamina
+}
+
 public class StoreData
 {
     public Define_Weapon.Weapons weapon;
-    public ItemSO itemData;
+    public Sprite icon;
+    public IsCharacterStat stat;
+    public List<ItemSO> itemDatas;
+    public List<bool> purchasedData;
 }
 
 public class StoreDataManager : MonoBehaviour
 {
     public static StoreDataManager Instance;
-    public List<StoreData> itemList;
+    public List<StoreData> itemList = new List<StoreData>();
     [SerializeField] private List<ItemSO> itemData;
+    [SerializeField] private List<WeaponSO> weaponData;
+    [SerializeField] private List<Sprite> weaponicon;
+
+    public int money = 1000000;
 
 
     void Start()
     {
         Instance = this;
-
-
+        StartCoroutine(InputInfo());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator InputInfo()
     {
-        
+        yield return new WaitForSeconds(0.1f);
+
+        for (int i = 0; i < System.Enum.GetValues(typeof(Define_Weapon.Weapons)).Length; i++)
+        {
+            StoreData storeData = new StoreData();
+            storeData.itemDatas = new List<ItemSO>();
+            storeData.purchasedData = new List<bool>();
+
+            storeData.weapon = (Define_Weapon.Weapons)i;
+            storeData.icon = weaponicon[i];
+            storeData.itemDatas.Add(itemData[0]);
+            storeData.purchasedData.Add(false);
+
+            storeData.itemDatas.Add(itemData[1]);
+            storeData.purchasedData.Add(false);
+            itemList.Add(storeData);
+
+            storeData = new StoreData();
+            storeData.itemDatas = new List<ItemSO>();
+            storeData.purchasedData = new List<bool>();
+
+            storeData.icon = weaponicon[i];
+            storeData.itemDatas.Add(itemData[2]);
+            storeData.purchasedData.Add(false);
+            itemList.Add(storeData);
+        }
+
+        /*
+        StoreData statData = new StoreData();
+        statData.itemDatas = new List<ItemSO>();
+        statData.purchasedData = new List<bool>();
+
+        statData.stat = IsCharacterStat.hp;
+        statData.itemDatas.Add(itemData[3]);
+        statData.purchasedData.Add(false);
+        statData.itemDatas.Add(itemData[4]);
+        statData.purchasedData.Add(false);
+        itemList.Add(statData);
+        */
+    }
+
+    public void OnChangeStat(int weaponNumber, ItemSO item)
+    {
+        if (weaponNumber==0)
+        {
+            if (item != null)
+            {
+                Managers.PlayerStats.WeaponStatApply(weaponData[weaponNumber], item);
+            }
+        }
+        else
+        {
+            Debug.Log("not done");
+        }
+    }
+
+    public bool OnChangeMoney(int cost)
+    {
+        if(money >cost)
+        {
+            money -= cost;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
 }
--- Assets/Scripts/KCW/UI_UpgradeControl.cs
+++ Assets/Scripts/KCW/UI_UpgradeControl.cs
@@ -1,21 +1,123 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UI_UpgradeControl : MonoBehaviour
 {
-    public List<GameObject> UISlotPrefabs = new List<GameObject>();
+    public List<GameObject> UISlotPrefabs = new List<GameObject>(); //2개 , 3개
     public List<GameObject> UISlot = new List<GameObject>();
 
-    // Start is called before the first frame update
+    public Transform parentObj;
+
+    public GameObject Information;
+
+    int ButtonCount=0;
+    int iconCount = 0;
+
+    
     void Start()
     {
+        foreach (var item in StoreDataManager.Instance.itemList)
+        {
+            
+            if(item.itemDatas != null)
+            {
+                int count = item.itemDatas.Count;
+                if(count == 1)
+                {
+                    GameObject Slot = Instantiate(UISlotPrefabs[0], parentObj);
+                    Slot.transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = item.icon;
+                    Slot.transform.GetChild(2).GetChild(0).GetComponent<Image>().sprite = item.icon;
+                    Slot.transform.GetChild(2).GetComponentInChildren<Text>().text = item.itemDatas[0].displayName;
+                    Slot.transform.GetChild(0).GetChild(0).GetComponent<Button>().onClick.AddListener(OnDisplay);
+                    Slot.transform.GetChild(2).GetChild(0).GetComponent<Button>().onClick.AddListener(OnDisplay);
+                    string temp = iconCount + "_" + ButtonCount;
+                    Slot.transform.GetChild(2).GetChild(0).name = temp;
+                    ButtonCount++;
+                    UISlot.Add(Slot);
+                }
+                else if(count == 2)
+                {
+                    GameObject Slot = Instantiate(UISlotPrefabs[1], parentObj);
+                    Slot.transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = item.icon;
+                    Slot.transform.GetChild(2).GetChild(0).GetComponent<Image>().sprite = item.icon;
+                    Slot.transform.GetChild(4).GetChild(0).GetComponent<Image>().sprite = item.icon;
+
+                    Slot.transform.GetChild(2).GetChild(1).GetComponent<Text>().text = item.itemDatas[0].displayName;
+                    Slot.transform.GetChild(4).GetChild(1).GetComponent<Text>().text = item.itemDatas[1].displayName;
+
+                    Slot.transform.GetChild(0).GetChild(0).GetComponent<Button>().onClick.AddListener(OnDisplay);
+                    Slot.transform.GetChild(2).GetChild(0).GetComponent<Button>().onClick.AddListener(OnDisplay);
+                    string temp = iconCount + "_" + ButtonCount;
+                    Slot.transform.GetChild(2).GetChild(0).name = temp;
+                    ButtonCount++;
+
+                    Slot.transform.GetChild(4).GetChild(0).GetComponent<Button>().onClick.AddListener(OnDisplay);
+                    temp = iconCount + "_" + ButtonCount;
+                    Slot.transform.GetChild(4).GetChild(0).name = temp;
+                    ButtonCount++;
+                    UISlot.Add(Slot);
+                }
+                iconCount++;
+            }
+            
+        }
+
+    }
+
+    public void OnPurchase()
+    {
+        int itemListNumber;
+        int itemDatasNumber;
+
+        string[] item = Information.transform.name.Split('_');
+
+        try
+        {
+            if (int.TryParse(item[0], out itemListNumber) && int.TryParse(item[1], out itemDatasNumber))
+            {
+                ItemSO itemToChange = StoreDataManager.Instance.itemList[itemListNumber].itemDatas[itemDatasNumber];
+                if (StoreDataManager.Instance.OnChangeMoney(itemToChange.cost))
+                {
+                    StoreDataManager.Instance.OnChangeStat(itemListNumber, itemToChange);
+                    Information.GetComponent<Button>().enabled = false;
+                }
+            }
+        }
+        catch
+        {
+
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDisplay()
     {
-        
+        int itemListNumber;
+        int itemDatasNumber;
+
+        GameObject gameObject = EventSystem.current.currentSelectedGameObject;
+        string[] item = gameObject.transform.name.Split('_');
+
+        try
+        {
+            if (int.TryParse(item[0], out itemListNumber) && int.TryParse(item[1], out itemDatasNumber))
+            {
+                Information.SetActive(true);
+                Information.GetComponentInChildren<Text>().text = StoreDataManager.Instance.itemList[itemListNumber].itemDatas[itemDatasNumber].description;
+                Information.GetComponentInChildren<Button>().onClick.AddListener(OnPurchase);
+                Information.name = gameObject.transform.name;
+            }
+        }
+        catch
+        {
+
+        }
     }
+
+
 }
